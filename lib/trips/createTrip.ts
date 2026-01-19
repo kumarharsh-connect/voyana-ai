97b5ab9fe9dea863047ba0d0b1ead4377c7f12e8
kumarharsh-connect <kumarharsh.connect@gmail.com>
2026-01-19 06:29:41 +0530

    Minor Bugs Fix

--- lib/trips/createTrip.ts
+++ lib/trips/createTrip.ts
@@ -55,7 +55,6 @@ export async function createTrip(input: CreateTripInput) {
       maxBudget: resolvedMaxBudget,
       currency: resolvedCurrency,
       status: 'GENERATED',
-      mapsEnriched: true,
     },
   });
 
